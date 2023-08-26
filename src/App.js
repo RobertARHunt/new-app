b6081142a88848c6a2fe35e555acc28191aa82b4
Robert Hunt <robertarhunt@gmail.com>
2023-08-26 20:11:23 +0100

    set up react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [gridState, setGridState] = useState(1)
+
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
